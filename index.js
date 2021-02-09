a7de030bccbcb412a73fcde943031d6ec7937050
Yimura <andreas.maerten@scarlet.be>
2021-02-09 23:42:23 +0100

    refactor(Index): RegisterCommands updated output

--- index.js
+++ index.js
@@ -40,9 +40,8 @@ export default class CommandRegistrar extends EventModule {
      * @private
      * @param {string} category The original category this bit was part of
      * @param {Object} bits The command category object within a category folder
-     * @param {string} [parentBit=''] Defaults to an empty string
      */
-    async _recursiveRegister(category, bits, parentBit = '') {
+    async _recursiveRegister(category, bits) {
         for (const bit in bits) {
             if (bits.hasOwnProperty(bit)) {
                 if (bits[bit] instanceof Promise) {
@@ -51,52 +50,63 @@ export default class CommandRegistrar extends EventModule {
 
                         const instance = new bits[bit](category, this._m);
                         if (instance.disabled) {
-                            this.log.warn('COMMANDS', `Command disabled: '${parentBit}${instance.name}'`);
+                            this.log.warn('COMMANDS', `Command disabled: "${instance.name}"`);
 
                             continue;
                         }
 
                         if (instance.permissions && instance.permissions.levels.filter(x => x.type === 'COMMAND_HANDLED').length == 1) {
                             if (!instance.permission || typeof instance.permission !== 'function') {
-                                this.log.error('COMMANDS', `Command '${parentBit}${instance.name}' has COMMAND_HANDLED permission set but doesn't handle these!`);
+                                this.log.error('COMMANDS', `Command "${instance.name}" has COMMAND_HANDLED permission set but doesn't handle these!`);
 
                                 continue;
                             }
                         }
 
-                        const name = `${parentBit}${instance.name}`;
-                        this.commandList.set(name, instance);
+                        this.commandList.set(instance.name, instance);
 
                         if (this.output && !instance.hidden) {
-                            if (parentBit.length == 0) {
-                                this.output[category].commands.push(instance.raw);
-                            }
-                            else {
-                                if (!this.output[category].children) this.output[category].children = {};
-                                if (!this.output[category].children[parentBit.trim()]) this.output[category].children[parentBit.trim()] = [];
-
-                                this.output[category].children[parentBit.trim()].push(instance.raw);
-                            }
+                            this._updateOutput(category, instance.raw);
                         }
                         else if (instance.hidden) {
-                            this.log.info('COMMANDS', `Command hidden: '${parentBit}${instance.name}'`);
+                            this.log.info('COMMANDS', `Command hidden: "${instance.name}"`);
                         }
 
                         for (const alias of instance.aliases) {
-                            this.commandList.set(`${parentBit}${alias}`, instance);
+                            this.commandList.set(alias, instance);
                         }
 
                         continue;
                     } catch (e) {
                         e.ignore = true;
 
-                        this.log.warn('COMMANDS', `The following command: ${parentBit}${bit}\nGenerated the following error:\n${e.stack}`);
+                        this.log.warn('COMMANDS', `The following command: ${bit}\nGenerated the following error:\n${e.stack}`);
                     }
                 }
 
-                await this._recursiveRegister(category, bits[bit], `${parentBit}${bit} `);
+                await this._recursiveRegister(category, bits[bit]);
             }
         }
+    }
+
+    /**
+     * @param {Object} commanObj 
+     */
+    _updateOutput(category, commanObj) {
+        if (!this.output[category]) this.output[category] = [];
+                            
+        this.output[category].push(commanObj);
+
+        clearTimeout(this.writeTimeout);
+        this.writeTimeout = setTimeout(() => {
+            fs.writeFile(resolve(`./data/commands.json`), JSON.stringify(this.output, null, '    '), { flag: 'w+' }, (err) => {
+                if (err) {
+                    throw err;
+                }
+
+                this.log.info('COMMANDS', 'Generated new "data/commands.json" with the mapped commands.');
+            });
+        }, 500);
     }
 
     async init() {
@@ -105,27 +115,11 @@ export default class CommandRegistrar extends EventModule {
         this.commandList = new CommandList();
         if (this.config.development && this.config.generate_command_json) this.output = {};
 
-        for (const categoryName in commands) {
-            if (commands.hasOwnProperty(categoryName)) {
-                if (this.output) this.output[categoryName] = { commands: [] };
-
+        for (const categoryName in commands)
+            if (commands.hasOwnProperty(categoryName))
                 await this._recursiveRegister(categoryName, commands[categoryName]);
-            }
-        }
 
         this.log.info('COMMANDS', `Mapping of commands done with ${this.commandList.registered} unique commands registered, ${this.commandList.size - this.commandList.registered} aliases registered.`);
-
-        if (this.output) {
-            fs.writeFile(resolve(`./data/commands.json`), JSON.stringify(this.output, null, '    '), { flag: 'w+' }, (err) => {
-                if (err) {
-                    throw err;
-                }
-
-                this.log.info('COMMANDS', 'Generated new "data/commands.json" with the mapped commands.');
-            });
-
-            delete this.output;
-        }
 
         this.globalStorage.set('prefix', this.config.development ? this.config.default_prefix.dev : this.config.default_prefix.prod);
 
